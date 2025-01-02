0f6d8ff5b7c00303aac0bdfd7a13b4befebb9bfc
parithosh <parithosh.jayanthi@ethereum.org>
2025-01-02 12:48:04 +0100

    updating plugin list check

--- setup.sh
+++ setup.sh
@@ -1,12 +1,42 @@
 #!/bin/bash
+set -e
 
 # Plugin list
-asdf plugin add age https://github.com/threkk/asdf-age.git
-asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
-asdf plugin add sops https://github.com/feniix/asdf-sops.git
-asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
-asdf plugin-add python
+if ! asdf plugin list | grep -q age; then
+  asdf plugin add age https://github.com/threkk/asdf-age.git
+fi
+
+if ! asdf plugin list | grep -q shellcheck; then
+  asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
+fi
+
+if ! asdf plugin list | grep -q sops; then
+  asdf plugin add sops https://github.com/feniix/asdf-sops.git
+fi
+
+if ! asdf plugin list | grep -q terraform; then
+  asdf plugin add terraform https://github.com/asdf-community/asdf-hashicorp.git
+fi
+
+if ! asdf plugin list | grep -q helm; then
+  asdf plugin add helm https://github.com/Antiarchitect/asdf-helm.git
+fi
+
+if ! asdf plugin list | grep -q python; then
+  asdf plugin add python
+fi
+
+if ! asdf plugin list | grep -q ansible-lint; then
+  asdf plugin add ansible-lint
+fi
+
+if ! asdf plugin list | grep -q awscli; then
+  asdf plugin add awscli
+fi
+
+if ! asdf plugin list | grep -q yq; then
+  asdf plugin add yq
+fi
 
 asdf install
 
