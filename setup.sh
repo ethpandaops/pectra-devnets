8a1125409c41180d02604345cb4abc6b4d16b3b4
parithosh <parithosh.jayanthi@ethereum.org>
2025-01-02 13:03:25 +0100

    test tf

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@
 asdf plugin add age https://github.com/threkk/asdf-age.git
 asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
 asdf plugin add sops https://github.com/feniix/asdf-sops.git
-asdf plugin-add terraform-ls https://github.com/asdf-community/asdf-hashicorp.git
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
 asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
 asdf plugin-add python
 
