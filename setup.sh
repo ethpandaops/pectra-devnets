7ad47d44c93bc522b51eefa20deed1ef8e47444d
Rafael Matias <rafael@skyle.net>
2025-01-28 10:17:26 +0100

    setup.sh: dont fail when plugins already exists

--- setup.sh
+++ setup.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -xe
 
 # Plugin list
-asdf plugin add age https://github.com/threkk/asdf-age.git
-asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git
-asdf plugin add sops https://github.com/feniix/asdf-sops.git
-asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git
-asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git
-asdf plugin-add python
+asdf plugin add age https://github.com/threkk/asdf-age.git || true
+asdf plugin add shellcheck https://github.com/luizm/asdf-shellcheck.git || true
+asdf plugin add sops https://github.com/feniix/asdf-sops.git || true
+asdf plugin-add terraform https://github.com/asdf-community/asdf-hashicorp.git || true
+asdf plugin-add helm https://github.com/Antiarchitect/asdf-helm.git || true
+asdf plugin-add python || true
 
 asdf plugin-list
 asdf install
