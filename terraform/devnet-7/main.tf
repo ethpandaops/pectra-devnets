a20365937c8ebdc0eee593f5c0d2a302b3dd1dc3
Rafael Matias <rafael@skyle.net>
2025-02-26 18:28:28 +0100

    change network range due to being used already

--- terraform/devnet-7/main.tf
+++ terraform/devnet-7/main.tf
@@ -54,7 +54,7 @@ variable "ethereum_network" {
 }
 
 variable "base_cidr_block" {
-  default = "10.77.0.0/16"
+  default = "10.87.0.0/16"
 }
 ////////////////////////////////////////////////////////////////////////////////////////
 //                                        LOCALS
